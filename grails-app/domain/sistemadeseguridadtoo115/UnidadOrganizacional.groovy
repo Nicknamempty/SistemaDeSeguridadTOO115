dffa58b71fe7d94a02fd400d3f3c524d74873a82
Edwin Edgardo Lopez <edgardolopez.170@gmail.com>
2020-11-20 20:46:03 -0600

    add Model

---
+++ grails-app/domain/sistemadeseguridadtoo115/UnidadOrganizacional.groovy
@@ -0,0 +1,19 @@
+package sistemadeseguridadtoo115
+
+class UnidadOrganizacional {
+    int nivel
+    String nombreUnidad
+    static belongsTo = [ subUnidadOrganizacional: UnidadOrganizacional ]
+    static mappedBy = [nivel: "none", nombreUnidad: "none"]
+    static constraints = {
+        nivel min: 0
+        nombreUnidad size: 3.. 100, unique:true
+        subUnidadOrganizacional nullable: true
+    }
+    static mapping = {
+        version false
+    }
+    String toString() {
+        nombreUnidad
+    }
+}
---
+++ src/test/groovy/sistemadeseguridadtoo115/UnidadOrganizacionalSpec.groovy
@@ -0,0 +1,18 @@
+package sistemadeseguridadtoo115
+
+import grails.testing.gorm.DomainUnitTest
+import spock.lang.Specification
+
+class UnidadOrganizacionalSpec extends Specification implements DomainUnitTest<UnidadOrganizacional> {
+
+    def setup() {
+    }
+
+    def cleanup() {
+    }
+
+    void "test something"() {
+        expect:"fix me"
+            true == false
+    }
+}
